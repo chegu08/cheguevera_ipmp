8ec1480e34faf7be4258ac119cab034fbf963308
chegu08 <143737377+chegu08@users.noreply.github.com>
2024-02-04 20:50:58 +0530

    Add files via upload

---
+++ product array without division.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    // nums: given vector
+    // return the Product vector P that hold product except self at each index
+    vector<long long int> productExceptSelf(vector<long long int>& nums, int n) {
+       
+        using ll=long long;
+        vector<ll> ans(n,1);
+        if(n==1) return ans;
+        vector<ll> left(n,1),right(n,1);
+        for(int i=1;i<n;i++)
+        {
+            left[i]=left[i-1]*nums[i-1];
+            right[n-i-1]=right[n-i]*nums[n-i];
+        }
+        for(int i=0;i<n;i++) ans[i]=left[i]*right[i];
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+ {
+    int t;  // number of test cases
+    cin>>t;
+    while(t--)
+    {
+        int n;  // size of the array
+        cin>>n;
+        vector<long long int> arr(n),vec(n);
+        
+        for(int i=0;i<n;i++)    // input the array
+        {
+            cin>>arr[i];
+        }
+        Solution obj;
+        vec = obj.productExceptSelf(arr,n);   // function call
+        
+        for(int i=0;i<n;i++)    // print the output
+        {
+            cout << vec[i] << " ";
+        }
+        cout<<endl;
+    }
+	return 0;
+}
+// } Driver Code Ends

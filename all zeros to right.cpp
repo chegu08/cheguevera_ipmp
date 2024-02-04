b66bd4ef5737c2e18210efe204f932d8689a697a
chegu08 <143737377+chegu08@users.noreply.github.com>
2024-02-04 21:42:01 +0530

    Add files via upload

---
+++ all zeros to right.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	void pushZerosToEnd(int arr[], int n) {
+	    int left=0,right=0;
+	    while(right<n)
+	    {
+	        
+	        if(arr[left]==0)
+	        {
+	            while(arr[right]==0) right++;
+	            if(right<n)
+	            swap(arr[left],arr[right]);
+	            
+	        }
+	        left++;
+	            right++;
+	    }
+	}
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.pushZerosToEnd(arr, n);
+        for (i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
